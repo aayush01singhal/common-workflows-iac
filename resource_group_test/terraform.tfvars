7c58bdc7f8c440927fefbf011671174c66ad5e85
Aayush Singhal <aayush.singhal@ssamarinepartners.com>
2026-03-31 01:49:11 -0700

    add

--- resource_group_test/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-resource_group = {
-  rg1 = {
-    resource_group_name     = "rg-production-test"
-    resource_group_location = "westus2"
-  }
-}
--- resource_group_test/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "resource_group" {
-  description = "A map of resource groups"
-  type = map(object({
-    resource_group_name     = string
-    resource_group_location = string
-  }))
-}
